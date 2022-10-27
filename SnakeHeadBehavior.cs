aeb88a47b254a4964d96607e8d125dbc57cf64d6
Sarmisuper <sarmilan3@gmail.com>
2022-10-27 22:31:10 +0200

    Added boolean to queue actions

--- SnakeHeadBehavior.cs
+++ SnakeHeadBehavior.cs
@@ -6,34 +6,40 @@ public class SnakeHeadBehavior : MonoBehaviour
 {
     
     public string direction;
+    public bool isQueuedAction;
     void Start()
     {
         direction = "Right";
+        isQueuedAction = false;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && direction!="Left" && direction!="Right")
+        if (!isQueuedAction)
         {
-            transform.position += new Vector3(-1, 0, 0);
-            direction = "Left";
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != "Left" && direction != "Right")
+            {
+                direction = "Left";
+                isQueuedAction = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != "Right" && direction != "Left")
+            {
+                direction = "Right";
+                isQueuedAction = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow) && direction != "Up" && direction != "Down")
+            {
+                direction = "Up";
+                isQueuedAction = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != "Down" && direction != "Up")
+            {
+                direction = "Down";
+                isQueuedAction = true;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && direction!="Right" && direction!="Left")
-        {
-            transform.position += new Vector3(1, 0, 0);
-            direction = "Right";
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) && direction!="Up" && direction!="Down")
-        {
-            transform.position += new Vector3(0, 1, 0);
-            direction = "Up";
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && direction!="Down" && direction!="Up")
-        {
-            transform.position += new Vector3(0, -1, 0);
-            direction = "Down";
-        } 
-        else if (Time.frameCount % 100 == 0)
+
+        if (Time.frameCount % 100 == 0)
         {
             switch (direction)
             {
@@ -46,6 +52,8 @@ public class SnakeHeadBehavior : MonoBehaviour
                 case "Down": transform.position += new Vector3(0, -1, 0);
                     break;
             }
+
+            isQueuedAction = false;
         }
     }
 }
